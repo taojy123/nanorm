894945909e957cbf57d2bf95734d629362f57c06
Taojy <taojy0325@thundersoft.com>
2014-07-03 16:38:46 +0800

    完成功能： 数据库连接全局设置 大于/小于/不等于关系查询 模糊匹配查询 Test case 一对多关联关系

--- nanorm.py
+++ nanorm.py
@@ -1,16 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import sqlite3
 
 
-DB_NAME = "test.db"
-cx = sqlite3.connect(DB_NAME)
+NANO_SETTINGS = {
+    "type" : "sqlite3",
+    "db_name" : "test.db",
+    "cx" : sqlite3.connect("test.db"),
+}
+
+
+def set_db_name(db_name):
+    NANO_SETTINGS["db_name"] = db_name
+    NANO_SETTINGS["cx"] = sqlite3.connect(db_name)
+
 
 
 class Field(object):
     field_type = ""
-    default = None
+    field_level = 0
+    default = ""
 
     def field_sql(self, field_name):
-        return "%s %s" % (field_name, self.field_type)
+        return '"%s" %s' % (field_name, self.field_type)
 
 
 class CharField(Field):
@@ -31,13 +44,23 @@ class BooleanField(Field):
         self.field_type = "boolean"
         self.default = default
 
+
+class ForeignKey(Field):
+    def __init__(self, model_class):
+        self.field_type = "foreignkey"
+        self.field_level = 1
+        self.model_class = model_class
+
+    def field_sql(self, field_name):
+        return '"%s" integer REFERENCES "%s" ("id")' % (field_name, self.model_class.__name__.lower())
         
+
+
         
 class Model(object):
 
     def __init__(self, rid=0, **kwargs):
         self.table_name = self.__class__.__name__.lower()
-        self.try_create_table()
         self.id = rid
         for name in self.field_names:
             field = getattr(self.__class__, name)
@@ -58,54 +81,38 @@ class Model(object):
         return self.__str__()
 
 
+
     @property
     def field_names(self):
-        field_names = []
+        names = []
         for name in dir(self.__class__):
             var = getattr(self.__class__, name)
             if isinstance(var, Field):
-                field_names.append(name)
-        return field_names
+                names.append(name)
+        return names
 
 
     @property
     def field_values(self):
-        return ["'%s'" % getattr(self, name) for name in self.field_names]
-
-
-
-    def try_create_table(self):
-        cu = cx.cursor()
-
-        sql = "select * from sqlite_master where type='table' AND name='%s';" % self.table_name
-        cu.execute(sql)
-        if cu.fetchall():
-            return
-
-        sql = "drop table if exists %s;" % self.table_name
-        cu.execute(sql)
-
-        fields_sql = "" 
+        values = []
         for name in self.field_names:
-            field = self.__class__
-            field = getattr(self.__class__, name)
-            field_sql = field.field_sql(name)
-            fields_sql += ", " + field_sql
-        sql = "create table %s ( id integer not null primary key %s );" % (self.table_name, fields_sql)
-      
-        cu.execute(sql)
-        cx.commit()
+            value = getattr(self, name)
+            if isinstance(value, Model):
+                value = value.id
+            values.append("'%s'" % value)
+        return values
+
 
 
     def insert(self):
-        cu = cx.cursor()
+        cu = NANO_SETTINGS["cx"].cursor()
 
         field_names_sql = ", ".join(self.field_names)
         field_values_sql = ", ".join(self.field_values)
 
         sql = "insert into %s(%s) values(%s)" % (self.table_name, field_names_sql, field_values_sql)
         cu.execute(sql)
-        cx.commit()
+        NANO_SETTINGS["cx"].commit()
 
         sql = "select id from %s order by id desc;" % self.table_name
         cu.execute(sql)
@@ -113,7 +120,7 @@ class Model(object):
 
 
     def update(self):
-        cu = cx.cursor()
+        cu = NANO_SETTINGS["cx"].cursor()
 
         name_value = []
         for name, value in zip(self.field_names, self.field_values):
@@ -122,7 +129,7 @@ class Model(object):
 
         sql = "update %s set %s where id = %d" % (self.table_name, name_value_sql, self.id)
         cu.execute(sql)
-        cx.commit()
+        NANO_SETTINGS["cx"].commit()
 
 
     def save(self):
@@ -130,13 +137,38 @@ class Model(object):
             self.update()
         else:
             self.insert()
+        return self
             
 
     def delete(self):
-        cu = cx.cursor()
+        cu = NANO_SETTINGS["cx"].cursor()
         sql = "delete from %s where id = %d" % (self.table_name, self.id)
         cu.execute(sql)
-        cx.commit()
+        NANO_SETTINGS["cx"].commit()
+
+
+    @classmethod
+    def try_create_table(cls):
+        table_name = cls.__name__.lower()
+
+        cu = NANO_SETTINGS["cx"].cursor()
+        sql = "select * from sqlite_master where type='table' AND name='%s';" % table_name
+        cu.execute(sql)
+        if not cu.fetchall():
+            sql = "drop table if exists %s;" % table_name
+            cu.execute(sql)
+
+            fields_sql = "" 
+            for name in dir(cls):
+                var = getattr(cls, name)
+                if isinstance(var, Field):
+                    field = var
+                    field_sql = field.field_sql(name)
+                    fields_sql += ", " + field_sql
+            sql = 'create table %s ( "id" integer not null primary key %s );' % (table_name, fields_sql)
+            cu.execute(sql)
+
+            NANO_SETTINGS["cx"].commit()
 
 
     @classmethod
@@ -158,7 +190,6 @@ class Model(object):
 
 
 
-
 class Query(object):
 
     def __init__(self, model_class):
@@ -170,7 +201,7 @@ class Query(object):
 
 
     def __str__(self):
-        return "%s_%s_%s" % (self.__class__.__name__, self.table_name, self.sql)
+        return "%s_%s_%s" % (self.__class__.__name__, self.table_name, self.query_sql)
 
 
     def __unicode__(self):
@@ -192,30 +223,37 @@ class Query(object):
 
 
     @property
-    def sql(self):
+    def query_sql(self):
         sql = "select * from %s where %s %s;" % (self.table_name, self.where_sql, self.order_sql)
         return sql
 
 
-    def filter(self, **kwargs):
+    def filter(self, operator="=", **kwargs):
         where_sql = self.where_sql
         for name, value in kwargs.items():
-            where_sql += " and %s='%s'" % (name, value)
+            if name in self.field_names + ["id"]:
+                if isinstance(value, Model):
+                    value = value.id
+                where_sql += " and %s %s '%s'" % (name, operator, value)
         query = self.__class__(self.model_class)
+        query.order_sql = self.order_sql
         query.where_sql = where_sql
         return query
 
 
     def order(self, field_name):
-        order_sql = field_name.replace("-", "")
+        order_sql = "order by " + field_name.replace("-", "")
         if field_name[0] == "-":
             order_sql += " desc"
-        return order_sql
+        query = self.__class__(self.model_class)
+        query.where_sql = self.where_sql
+        query.order_sql = order_sql
+        return query
 
 
     def all(self):
-        cu = cx.cursor()
-        cu.execute(self.sql)
+        cu = NANO_SETTINGS["cx"].cursor()
+        cu.execute(self.query_sql)
         rows = cu.fetchall()
         obs = []
         for r in rows:
@@ -223,7 +261,16 @@ class Query(object):
             ob = self.model_class(rid=rid)
             for i in range(1, len(r)):
                 name = self.field_names[i-1]
-                value = r[i]
+                field = getattr(self.model_class, name)
+                if field.field_level == 0:
+                    if field.field_type == "boolean":
+                        value = eval(r[i])
+                    else:
+                        value = r[i]
+                elif field.field_level ==1:
+                    if field.field_type == "foreignkey":
+                        fid = r[i]
+                        value = field.model_class.get(id=fid)
                 setattr(ob, name, value)
             obs.append(ob)
         return obs
@@ -236,11 +283,12 @@ class Query(object):
 
 
     def delete(self):
-        cu = cx.cursor()
+        cu = NANO_SETTINGS["cx"].cursor()
         sql = "delete from %s where %s" % (self.table_name, self.where_sql)
         cu.execute(sql)
-        cx.commit()
+        NANO_SETTINGS["cx"].commit()
 
 
 
 
+# THE END
--- test.py
+++ test.py
@@ -1,36 +1,122 @@
 from nanorm import *
 
-class Student(Model):
+
+class Area(Model):
+    name = CharField()
+
+
+class Employee(Model):
     name = CharField(128)
     age = IntegerField(default=20)
     sex = BooleanField()
+    area = ForeignKey(Area)
 
     def __str__(self):
         return "%s_%s_%s_%s_%s" % (self.__class__.__name__, self.id, self.name, self.age, self.sex)
 
-s = Student()
-Student.query().delete()
 
-s1 = Student()
+
+# ==============================================
+
+set_db_name("nanorm.db")
+
+Area.try_create_table()
+Employee.try_create_table()
+
+Area.query().delete()
+Employee.query().delete()
+
+# ==============================================
+
+
+mainland = Area(name="mainland").save()
+taiwan = Area(name="taiwan").save()
+
+assert len(Area.gets()) == 2
+
+# ==============================================
+
+s1 = Employee()
 s1.name = "Joe"
 s1.age = 45
 s1.sex = True
+s1.area = taiwan
 s1.save()
 
-s2 = Student(name="Motive")
+joe = Employee.get(age=45)
+
+assert joe.name == "Joe"
+
+
+# ==============================================
+
+
+s2 = Employee(name="Motive")
 s2.age = 40
+s2.area = mainland
+s2.save()
+s2.area = taiwan
 s2.save()
 
-s3 = Student(name="Sandy", sex=False)
+motive = Employee.query().filter(id=2).all()[0]
+
+assert motive.area.name == "taiwan"
+
+
+# ==============================================
+
+
+s3 = Employee(name="Sandy", sex=False, area=mainland)
 s3.save()
 
-
-sandy = Student.query().filter(name="Sandy").first()
+sandy = Employee.query().filter(name="Sandy").first()
 sandy.age = 32
 sandy.save()
 
-print sandy
+sandy = Employee.gets(age=32)[0]
 
-print Student.gets(name="Joe")
+assert sandy.sex == False
 
-print "Finish!"
+
+# ==============================================
+
+s = Employee.query().order("age").all()
+
+assert s[0].name == "Sandy"
+
+# ==============================================
+
+s = Employee.query().filter(sex=True).order("-name").all()
+
+assert s[1].name == "Joe"
+
+# ==============================================
+
+s = Employee.get(age="32", operator="<=")
+
+assert s.name == "Sandy"
+
+# ==============================================
+
+s = Employee.get(name="J%", operator="like")
+
+assert s.name == "Joe"
+
+# ==============================================
+
+s = Employee.get(area=mainland)
+
+assert s.name == "Sandy"
+
+# ==============================================
+
+
+
+
+
+
+
+# ==============================================
+
+print Employee.gets()
+print "Success!"
