18f4160a856a83a3fb0ad4f7fdde9d2479ac259e
taojy123 <taojy123@163.com>
2016-04-09 14:10:32 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='nanorm',
       py_modules=['nanorm', 'nanorm_test'],
-      version='1.8',
+      version='1.8.1',
       keywords='orm namo mini sample database sqlite nanorm nanoorm',
       description='A simple ORM framework for Python ( Nano ORM )',
       long_description='This is a streamlined Python ORM framework for SLQite3. It intended to use a file write ORM, provide some basic functions develop when small or micro projects.',
