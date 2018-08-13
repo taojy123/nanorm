7b768cd86842aba8c871b8b50c202c1907e22960
taojy123 <taojy123@163.com>
2018-08-13 10:42:44 +0800

    improve setup

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
 from setuptools import setup
 import nanorm
 
-setup(name='nanorm',
-      py_modules=['nanorm', 'nanorm_test'],
-      version=nanorm.__VERSION__,
-      keywords='orm namo mini sample database sqlite nanorm nanoorm',
-      description='A simple ORM framework for Python ( Nano ORM )',
-      long_description=open('description.txt').read(),
-      author="Tao Jiayuan",
-      author_email="taojy123@163.com",
-      license="MIT",
-      url="https://github.com/taojy123/nanorm",
-      )
+
+try:
+    long_description = open('README.md').read()
+except Exception as e:
+    long_description = ''
+
+
+setup(
+    name='nanorm',
+    py_modules=['nanorm', 'nanorm_test'],
+    version=nanorm.__VERSION__,
+    keywords='orm namo mini sample database sqlite nanorm nanoorm',
+    description='A simple ORM framework for Python ( Nano ORM )',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Tao Jiayuan",
+    author_email="taojy123@163.com",
+    license="MIT",
+    url="https://github.com/taojy123/nanorm",
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
 
